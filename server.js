cd1d70bc9acd18f01acfbea1c60635cce8d9457f
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-02-02 16:32:16 +0530

    User Model & Hashing password

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute";
 
 // dotenv config
 dotenv.config();
@@ -15,6 +16,9 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRoutes);
 
 // rest api
 app.get("/", (req, res) => {
