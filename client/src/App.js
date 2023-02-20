2f4fbed96c2ddbbbe3f75c21db0f9de827dbb358
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-02-20 17:07:06 +0530

    Login/Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 import Contact from "./pages/Contact";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
@@ -10,6 +12,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/policy" element={<Policy />}></Route>
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      <main style={{ minHeight: "80vh" }}>{children}</main>
+      <main style={{ minHeight: "80vh" }}>
+        <Toaster />
+        {children}
+      </main>
       <Footer />
     </div>
   );
